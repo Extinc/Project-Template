6d8d3bc7ed380d636c493bf910e0ddb49d1d67ba
Lim Kei Yiang <angelic.noted_0g@icloud.com>
2025-03-29 15:17:25 +0800

    feat: added a new template for multi project gradle build
    using buildSrc and gradle's version catalogs

---
+++ Sample-Multi-Project-Build/projects/spring-proj-1/src/main/java/org/personal/springproj1/SpringProj1Application.java
@@ -0,0 +1,16 @@
+package org.personal.springproj1;
+
+import ch.qos.logback.classic.Logger;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringProj1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringProj1Application.class, args);
+		
+
+	}
+
+}
---
+++ Sample-Multi-Project-Build/projects/spring-proj-1/src/test/java/org/personal/springproj1/SpringProj1ApplicationTests.java
@@ -0,0 +1,14 @@
+package org.personal.springproj1;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringProj1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
